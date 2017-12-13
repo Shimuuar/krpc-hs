33664f2017658e35d5b55fbbbaeb7ea77009a2d9
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2017-12-13 23:36:44 +0300

    Do not keep files generated from PB in the source tree
    Instead add *.proto files and script for generating them

---
+++ KRPC/generate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# syntax "proto3" is not supported so we have to use VEGY ugly sed
+# hacks
+#
+# Script is expected to be run from main directory
+set -e
+[ -f krpc-hs.cabal ] || { echo "Not in root of project"; exit 1; }
+
+# Run
+VER=${1:-0.3.11}
+PROTO=KRPC/"$VER"/krpc.proto
+TEMP=KRPC/krpc.proto
+sed -re 's/^  ([A-Z]|string|bytes|uint|int|float|double|bool)/  optional \1/' \
+    $PROTO > $TEMP
+
+rm -rfv src/PB
+hprotoc --prefix=PB --haskell_out=src $TEMP
+rm $TEMP
